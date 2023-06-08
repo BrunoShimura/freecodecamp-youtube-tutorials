d0fc62f9ecea9f4acf16144d9121d1e561d3f939
Bruno Shimura <bruno_shimura@hotmail.com>
2023-06-07 21:20:40 -0300

    variables

--- c-programming-tutorial-for-beginners/Giraffe/main.c
+++ c-programming-tutorial-for-beginners/Giraffe/main.c
@@ -3,10 +3,12 @@
 
 int main()
 {
-    printf("    /|\n");
-    printf("   / |\n");
-    printf("  /  |\n");
-    printf(" /   |\n");
-    printf("/____|\n");
+    char characterName[] = "Lorem Ipsum";
+    int characterYear = 1500;
+
+    printf("%s is simply dummy text of the printing and typesetting\n", characterName);
+    printf("industry. %s has been the industry's standard dummy\n", characterName);
+    printf("text ever since the %ds, when an unknown printer took a galley\n", characterYear);
+    printf("of type and scrambled it to make a type specimen book. It has\n");
     return 0;
 }
