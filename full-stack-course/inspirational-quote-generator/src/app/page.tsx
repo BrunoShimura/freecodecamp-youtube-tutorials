5f8b1f435092cbe17130882c268b8063828275ae
Bruno Shimura <bruno_shimura@hotmail.com>
2023-07-18 16:03:45 -0300

    full stack course

---
+++ full-stack-course/inspirational-quote-generator/src/app/page.tsx
@@ -0,0 +1,13 @@
+import { GradientBackgroundCon } from "../../components/QuoteGenerator/QuoteGeneratorElements";
+
+export default function Home() {
+  return (
+    <>
+      {/* Background */}
+      
+      <GradientBackgroundCon>
+        <h1>Hello</h1>
+      </GradientBackgroundCon>
+    </>
+  )
+}
