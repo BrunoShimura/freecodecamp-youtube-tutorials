1add23417f80f826149c163e25b94b39b4fbd625
Bruno Shimura <bruno_shimura@hotmail.com>
2023-07-19 13:45:23 -0300

    create a footer for database data

--- full-stack-course/inspirational-quote-generator/src/app/page.tsx
+++ full-stack-course/inspirational-quote-generator/src/app/page.tsx
@@ -1,12 +1,41 @@
-import { GradientBackgroundCon } from "../../components/QuoteGenerator/QuoteGeneratorElements";
+'use client'
+
+import React, { useState } from 'react';
+
+// Components
+import { BackgroundImage1, BackgroundImage2, FooterCon, FooterLink, GradientBackgroundCon, RedSpan } from "../../components/QuoteGenerator/QuoteGeneratorElements";
+
+//Assets
+import Clouds1 from "../../assets/cloud-and-thunder.png"
+import Clouds2 from "../../assets/cloudy-weather.png"
 
 export default function Home() {
+  const [numberOfQuotes, setNumberOfQuotes] = useState<Number | null>(0);
   return (
     <>
       {/* Background */}
       
       <GradientBackgroundCon>
-        <h1>Hello</h1>
+        <BackgroundImage1
+          src={Clouds1}
+          height="300"
+          alt="cloudybackground1"
+        />
+        <BackgroundImage2
+          src={Clouds2}
+          height="300"
+          alt="cloudybackground2"
+        />
+        <FooterCon>
+          <>
+            Quotes generated: {numberOfQuotes}
+            <br />
+            Developed with <RedSpan>❤</RedSpan> by <FooterLink 
+            href="http://" 
+            target="_blank" 
+            rel="noopener noreferrer">@Shimura</FooterLink>
+          </>
+        </FooterCon>
       </GradientBackgroundCon>
     </>
   )
