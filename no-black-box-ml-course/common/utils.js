e9958e1ecac22c9110bf677616d7959899d7b8cb
Bruno Anthony Shimura <bruno_shimura@hotmail.com>
2023-09-01 15:07:06 -0300

    scatter plot

--- no-black-box-ml-course/common/utils.js
+++ no-black-box-ml-course/common/utils.js
@@ -5,14 +5,14 @@ utils.formatPercent = (n) => {
 }
 
 utils.styles = {
-    car: 'gray',
-    fish: 'red',
-    house: 'yellow',
-    tree: 'green',
-    bicycle: 'cyan',
-    guitar: 'blue',
-    pencil: 'magenta',
-    clock: 'lightgray'
+    car: {color: 'gray', text: '🚗'},
+    fish: {color: 'red', text: '🐟'},
+    house: {color: 'yellow', text: '🏠'},
+    tree: {color: 'green', text: '🌲'},
+    bicycle: {color: 'cyan', text: '🚲'},
+    guitar: {color: 'blue', text: '🎸'},
+    pencil: {color: 'magenta', text: '✏️'},
+    clock: {color: 'lightgray', text: '⏰'}
 }
 
 utils.printProgress = (count, max) => {
