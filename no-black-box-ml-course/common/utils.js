2c44bd9192655aeaa13b9d39c8a2a197dad501b3
Bruno Anthony Shimura <bruno_shimura@hotmail.com>
2023-09-05 14:02:35 -0300

    k nearest neighbors classifier

--- no-black-box-ml-course/common/utils.js
+++ no-black-box-ml-course/common/utils.js
@@ -45,20 +45,15 @@ utils.distance=(p1,p2)=>{
     );
 }
 
-utils.getNearest=(loc,points)=>{
-    let minDist=Number.MAX_SAFE_INTEGER;
-    let nearestIndex=0;
-
-    for(let i=0;i<points.length;i++){
-    const point=points[i];
-    const d=utils.distance(loc,point);
-
-    if(d<minDist){
-        minDist=d;
-        nearestIndex=i;
-    }
-    }
-    return nearestIndex;
+utils.getNearest=(loc,points, k=1)=>{
+    const obj = points.map((val, ind) => {
+        return {ind, val}
+    });
+    const sorted = obj.sort((a, b) => {
+        return utils.distance(loc, a.val) - utils.distance(loc, b.val)
+    });
+    const indices = sorted.map((obj) => obj.ind);
+    return indices.slice(0, k);
 }
 
 utils.invLerp = (a, b, v) => {
